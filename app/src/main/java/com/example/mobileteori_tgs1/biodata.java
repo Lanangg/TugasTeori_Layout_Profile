d5ebc36fa24c94f84603254e7875fdb09461c106
Lanang <2018072@scholar.itn.ac.id>
2022-10-20 10:04:24 +0700

    UI Profile 2018072

---
+++ app/src/main/java/com/example/mobileteori_tgs1/biodata.java
@@ -0,0 +1,14 @@
+package com.example.mobileteori_tgs1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class biodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+}
